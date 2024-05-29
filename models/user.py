79b0a7a673ba4c977444c9c8c687464a09497d25
trOyYey <troyzuwara@gmail.com>
2024-05-29 01:00:34 +0000

    user password

--- models/user.py
+++ models/user.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ holds class User"""
+import hashlib
 import models
 from models.base_model import BaseModel, Base
 from os import getenv
@@ -26,4 +27,10 @@ class User(BaseModel, Base):
 
     def __init__(self, *args, **kwargs):
         """initializes user"""
+        if "password" in kwargs:
+            kwargs["password"] = self.hash(kwargs["password"])
         super().__init__(*args, **kwargs)
+
+    def hash(self, pwd):
+        """hashing user password"""
+        return hashlib.md5(pwd.encode()).hexdigest()
